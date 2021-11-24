c775d8631914f97bf697506ca050c5177eda9d59
Jason Scharf <jasonscharf3@gmail.com>
2021-11-24 12:34:31 -0500

    Added footer and revised the header.

---
+++ src/components/LowerFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-footer id="jasonFooter">
+      
+    <v-card class="flex" flat tile
+    color="primary">
+
+      <v-card-text class="py-4 black--text text-center">
+        {{ new Date().getFullYear() }} — <strong>Jason D Scharf Designs</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-email", "mdi-github", "mdi-twitter", "mdi-linkedin"]
+  })
+};
+</script>
+
+<style scoped>
+/* .flex {
+  background: primary;
+} */
+#jasonFooter {
+    margin-top: 300px;
+}
+</style>
